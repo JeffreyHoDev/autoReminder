3d4dba413594015ef12aa66433b86f364fc8cf74
Ho940106 <58864637+Ho940106@users.noreply.github.com>
2021-03-09 18:15:45 +0800

    send photo with telegram

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-// const TG = require('telegram-bot-api')
-// const fetch = require('node-fetch')
-
-// const api = new TG({
-//     token: "1689127461:AAF50wwFnlT7ep0B1Mm4eDfxtQRZL-WBTJo"
-// })
-
-// api.getMe()
-// .then(console.log)
-// .catch(console.err)
-
-// api.sendMessage()
-// .then(console.log)
-// .catch(console.err)
-
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1689127461:AAF50wwFnlT7ep0B1Mm4eDfxtQRZL-WBTJo';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.sendMessage('1689127461', 'Received your message: Hi there');
+var cron = require('node-cron');
+ 
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
+  bot.sendMessage('1474733292', 'Hello, there! Testing here')
+  var photoUrl = __dirname+'\\pic1.jpg';
+  console.log(photoUrl)
+//   bot.sendPhoto({
+//     chatId: '1474733292',
+//     caption: 'Test caption',
+//     photo: photoUrl
+//   }, function(err, msg) {
+//     console.log(err);
+//     console.log(msg);
+//   });
+    bot.sendPhoto('1474733292', photoUrl, {caption: "I'm a bot!"})
+    .then(console.log)
+    .catch(console.error)
+});
+
+
